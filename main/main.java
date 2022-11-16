cdba9d11c6127de0610e5eabb9e96057530334a5
rajanish-1631 <118096701+rajanish-1631@users.noreply.github.com>
2022-11-16 18:56:15 +0530

    Create main.java

---
+++ main/main.java
@@ -0,0 +1,13 @@
+package Category_Details;
+
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class Category_Product_main {
+
+	public static void main(String[] args) {
+		
+SpringApplication.run(Category_Product_main.class, args);
+	}
+
+}
